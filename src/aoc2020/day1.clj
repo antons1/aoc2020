31d8e00151c14636074a7bc1f6b5950bd4f33d33
Håkon Antonsen <haakon@antons1.net>
2020-12-01 13:25:55 +0100

    Day 1 - Part 2

--- src/aoc2020/day1.clj
+++ src/aoc2020/day1.clj
@@ -1,6 +1,7 @@
 (ns aoc2020.day1
   (:require [clojure.java.io :as io]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [clojure.math.combinatorics :as c]))
 
 (defn read-input [filename]
   (->> (io/resource filename)
@@ -8,27 +9,20 @@
     (s/split-lines)
     (map #(read-string %))))
 
-(defn sum=2020? [a b]
-  (= 2020 (+ a b)))
+(defn sum=2020? [nums]
+  (= 2020 (apply + nums)))
 
-(defn compare-with-vector [number, numbers]
-  (loop [current (first numbers)
-         remaining (rest numbers)]
-    (if (nil? current) current
-                       (if (sum=2020? number current) current
-                                                      (recur (first remaining) (rest remaining))))))
-
-(defn find-2020 [numbers]
-  (loop [current (first numbers)
-         remaining (rest numbers)]
-    (if (nil? current) current
-                       (if-let [is2020 (compare-with-vector current numbers)]
-                         (vector current is2020)
-                         (recur (first remaining) (rest remaining))))))
+(defn find-2020 [combinations]
+  (->> combinations
+       (filter #(sum=2020? %))))
 
 (comment
   (read-input "day1.txt")
-  (sum=2020? 1010 1011)
-  (find-2020 (read-input "day1.txt"))
-  (compare-with-vector 1024 (read-input "day1.txt"))
-  (apply * (find-2020 (read-input "day1.txt"))))
+  (as-> (read-input "day1.txt") nums
+      (c/combinations nums 3)
+      (find-2020 nums)
+      (first nums)
+      (apply * nums)))
+
+
+
